54f287ec766401c264bb92905451f7dca94192c3
Tanvir Rubayed <tanvirrubayedtauhid@gmail.com>
2021-02-14 00:37:03 +0600

    set timeout.js

---
+++ timeout.js
@@ -0,0 +1,14 @@
+function doSomeThing() {
+    console.log(2222);
+}
+
+console.log(1111);
+// setTimeout(() => {
+//     console.log('see you later');
+// }, 1000);
+
+setInterval(() => {
+    console.log('hey set interval');
+}, 1000);
+console.log(3333);
+console.log(4444);
